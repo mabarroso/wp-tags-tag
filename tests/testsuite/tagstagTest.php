9771fb5e85f1f9e69830661bc9bcd43c9f35e5f1
mabarroso <mabarroso@mabarroso.com>
2013-04-10 21:28:45 +0200

    Aceptance tests

--- tests/testsuite/tagstagTest.php
+++ tests/testsuite/tagstagTest.php
@@ -13,6 +13,36 @@ class TagsTagTest extends WP_UnitTestCase {
       $sqlLine && $wpdb->query($sqlLine);
     }
 
-    $this->categoriesTag = $GLOBALS['TagsTag'];
+    $this->tagsTag = $GLOBALS['TagsTag'];
+  }
+
+  public function testDefault() {
+    $response = $this->tagsTag->append_content('[tags]');
+    $this->assertContains('tag 1</a>',  $response);
+    $this->assertContains('tag 1.1',    $response);
+    $this->assertContains('tag 1.2',    $response);
+    $this->assertContains('tag 1.3',    $response);
+    $this->assertContains('tag 2</a>',  $response);
+    $this->assertContains('tag 2.1',    $response);
+    $this->assertContains('tag 2.2',    $response);
+    $this->assertContains('tag 2.3',    $response);
+    $this->assertContains('tag 3</a>',  $response);
+    $this->assertContains('tag 3.1',    $response);
+    $this->assertContains('tag 3.2',    $response);
+    $this->assertContains('tag 3.3',    $response);
+  }
+
+  public function testShowCount() {
+    $response = $this->tagsTag->append_content('[tags show_count="yes"]');
+    $this->assertContains('tag 2.1</a> (21)', $response);
+    $this->assertContains('tag 2.2</a> (22)', $response);
+    $this->assertContains('tag 2.3</a> (0)', $response);
+  }
+
+  public function testHideEmpty() {
+    $response = $this->tagsTag->append_content('[tags hide_empty="yes"]');
+    $this->assertContains('tag 2.1', $response);
+    $this->assertContains('tag 2.2', $response);
+    $this->assertNotContains('tag 2.3', $response);
   }
 }
